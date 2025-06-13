389ea429f9f8df2b6b54282dcc763ebf252a700d
stadia03 <gaurab.working@gmail.com>
2025-06-14 00:14:32 +0530

    cors setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,25 @@ import verifyToken from './middleware/verifyToken';
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({
   origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
+
+app.use((req, res, next)  => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+     res.sendStatus(200);
+     return;
+  }
+  next();
+});
+
+app.options('*', cors()); // Preflight handling
+
 
 
 app.get('/',async(req,res)=>{
