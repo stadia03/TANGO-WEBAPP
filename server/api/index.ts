4141f0c1656dcbf0487632c728e040aad8dcfe8b
stadia03 <gaurab.working@gmail.com>
2025-06-17 13:47:26 +0530

    delete app/index

--- server/api/index.ts
+++
@@ -1,4 +0,0 @@
-import serverlessExpress from '@vendia/serverless-express';
-import app from '../src/index' // Adjust path as per your structure
-
-export const handler = serverlessExpress({ app });
