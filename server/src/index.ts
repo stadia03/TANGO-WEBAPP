b48de3ee56abe2235812367fff95c2c3ef2ffaab
stadia03 <gaurab.working@gmail.com>
2025-06-18 01:17:39 +0530

    added support for local dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
-import 'dotenv/config';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
 
-import userRoutes from './routes/user';
-import authRoutes from './routes/auth';
-import adminRoutes from './routes/admin';
-import verifyToken from './middleware/verifyToken';
-import dbConnect from './utils/db';
+import userRoutes from "./routes/user";
+import authRoutes from "./routes/auth";
+import adminRoutes from "./routes/admin";
+import verifyToken from "./middleware/verifyToken";
+import dbConnect from "./utils/db";
 
 const app = express();
 let isConnected = false;
@@ -15,10 +15,21 @@ app.use(cors());
 app.use(express.json());
 
 // Routes setup
-app.get('/', (_req: Request, res: Response) => {res.send('server working!')});
-app.use('/auth', authRoutes);
-app.use('/user', verifyToken('user'), userRoutes);
-app.use('/admin', verifyToken('admin'), adminRoutes);
+app.get("/", (_req: Request, res: Response) => {
+  res.send("server working!");
+});
+app.use("/auth", authRoutes);
+app.use("/user", verifyToken("user"), userRoutes);
+app.use("/admin", verifyToken("admin"), adminRoutes);
+
+// const localhost = async () => {
+//   await dbConnect();
+//   app.listen(3500,() => {
+//     console.log("Server started!");
+//   });
+// };
+
+// localhost();
 
 // Vercel handler
 export default async function handler(req: Request, res: Response) {
@@ -26,5 +37,6 @@ export default async function handler(req: Request, res: Response) {
     await dbConnect();
     isConnected = true;
   }
+
   return app(req, res);
 }
